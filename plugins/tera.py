fd7f28ad20296640d51af43d1844af7f608f4904
NYBotx <167092531+NYBotx@users.noreply.github.com>
2025-06-20 20:01:36 +0530

    Update tera.py

--- plugins/tera.py
+++ plugins/tera.py
@@ -27,7 +27,7 @@ last_upload_col = db["terabox_lastupload"]
 
 TERABOX_REGEX = r'https?://(?:www\.)?[^/\s]*tera[^/\s]*\.[a-z]+/s/[^\s]+'
 
-COOKIE = "ndus=Yq56lhKteHuiwmhJLW7QyFK20cJp2ZGjmx21nYJp" # add your own cookies like ndus=YzrYlCHteHuixx7IN5r0ABCDFXDGSTGBDJKLBKMKH
+COOKIE = "ndus=YzrYlCHteHuixx7IN5r0fc3sajSOYAHfqDoPM0dP" # add your own cookies like ndus=YzrYlCHteHuixx7IN5r0ABCDFXDGSTGBDJKLBKMKH
 
 HEADERS = {
     "Accept": "application/json, text/plain, */*",
